279b7022ed77e139b28a201b175b07a0c549e61c
BaBeuloula <babeuloula@gmail.com>
2019-08-31 15:10:38 +0200

    Create pull requests display

---
+++ src/Enum/Label.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author BaBeuloula <info@babeuloula.fr>
+ */
+
+declare(strict_types=1);
+
+namespace App\Enum;
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * @method static Label REVIEW_NEEDED()
+ * @method static Label CHANGES_REQUESTED()
+ * @method static Label ACCEPTED()
+ * @method static Label WIP()
+ */
+class Label extends Enum
+{
+    /** @var string */
+    protected const REVIEW_NEEDED = 'Review needed';
+
+    /** @var string */
+    protected const CHANGES_REQUESTED = 'Changes requested';
+
+    /** @var string */
+    protected const ACCEPTED = 'Accepted';
+
+    /** @var string */
+    protected const WIP = 'WIP';
+}
