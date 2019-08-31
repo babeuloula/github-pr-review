2463caa706b6ac22272f4e9f6ccd1e0a63e13464
BaBeuloula <babeuloula@gmail.com>
2019-08-31 19:17:40 +0200

    Use PHP Typed Array & update display

---
+++ src/TypedArray/Type/User.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author BaBeuloula <info@babeuloula.fr>
+ */
+declare(strict_types=1);
+
+namespace App\TypedArray\Type;
+
+class User
+{
+    /** @var string */
+    protected $login;
+
+    /** @var string */
+    protected $avatarUrl;
+
+    public function __construct(array $data)
+    {
+        $this->login = $data['login'];
+        $this->avatarUrl = $data['avatar_url'];
+    }
+
+    public function getLogin(): string
+    {
+        return $this->login;
+    }
+
+    public function getAvatarUrl(): string
+    {
+        return $this->avatarUrl;
+    }
+}
