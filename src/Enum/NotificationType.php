aa17e4e2e7f4482482920a10d4b9e2d157c70e31
Thomas <leone.thomas.92@gmail.com>
2019-10-06 17:21:05 +0200

    Add NotificationType::REPOSITORY_VULNERABILITY_ALERT (#34)
    * added repositoryVulnerabilityAlert to notifications
    
    * added @method static in PHPdoc

--- src/Enum/NotificationType.php
+++ src/Enum/NotificationType.php
@@ -12,6 +12,7 @@ use MyCLabs\Enum\Enum;
 /**
  * @method static NotificationType ISSUE()
  * @method static NotificationType PULL_REQUEST()
+ * @method static NotificationType REPOSITORY_VULNERABILITY_ALERT()
  */
 class NotificationType extends Enum
 {
@@ -20,4 +21,7 @@ class NotificationType extends Enum
 
     /** @var string */
     protected const PULL_REQUEST = 'PullRequest';
+
+    /** @var string  */
+    protected const REPOSITORY_VULNERABILITY_ALERT = 'RepositoryVulnerabilityAlert';
 }
