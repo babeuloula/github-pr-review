792691c7bebd9ca27fa33240a73b4cd97fd83723
ArthurHoaro <arthur.hoareau@wizacha.com>
2019-10-07 09:57:13 +0200

    Add undefined notification type to project's Enum

--- src/Enum/NotificationType.php
+++ src/Enum/NotificationType.php
@@ -10,17 +10,25 @@ namespace App\Enum;
 use MyCLabs\Enum\Enum;
 
 /**
+ * @method static NotificationType COMMIT()
  * @method static NotificationType ISSUE()
  * @method static NotificationType PULL_REQUEST()
+ * @method static NotificationType RELEASE()
  * @method static NotificationType REPOSITORY_VULNERABILITY_ALERT()
  */
 class NotificationType extends Enum
 {
     /** @var string */
+    protected const COMMIT = 'Commit';
+
+    /** @var string */
     protected const ISSUE = 'Issue';
 
     /** @var string */
     protected const PULL_REQUEST = 'PullRequest';
+
+    /** @var string */
+    protected const RELEASE = 'Release';
 
     /** @var string  */
     protected const REPOSITORY_VULNERABILITY_ALERT = 'RepositoryVulnerabilityAlert';
